179b0ef2bf14ea5e83d7b1be56c2620bff5035d2
madparcival <haarishkevin@gmail.com>
2023-03-22 21:08:07 +0530

    GPA calculations

--- calculation_class.py
+++ calculation_class.py
@@ -2,26 +2,20 @@
 class calculations:
     
     def calculateGPA(marksDict,semester):
-        for i in marksDict[f"semester_{semester}"]:
-            lengthOfPart=len(marksDict[f"semester_{semester}"][i])# TODO: we cant take the length of parts here because part contains mark,credit and grade
-            if  lengthOfPart>1:
-                for j in range(1,lengthOfPart+1):
-                    print(i)
-                    mark=marksDict[f"semester_{semester}"][i][f'mark_{j}']
-                    credit=marksDict[f"semester_{semester}"][i][f'credit_{j}']
-                    grade=marksDict[f"semester_{semester}"][i]['grade']
-                    calculatedGrade=(mark*credit)/credit
-                    marksDict[f"semester_{semester}"][i]['grade']=grade+calculatedGrade
-            else:
-                mark=marksDict[f"semester_{semester}"][i]['mark_1']
-                credit=marksDict[f"semester_{semester}"][i]['credit_1']
-                calculatedGrade=(mark*credit)/credit
-                marksDict[f"semester_{semester}"][i]['grade']=calculatedGrade
-            
-            
-            # for i in range(1,numberOfSub+1):
-            #     mark=marksDict[f"part_{part}"][f"mark_{i}"]
-            #     credit=marksDict[f"part_{part}"][f"credit_{i}"]
-            #     grade=(mark*credit)/credit
 
-            #     marksDict[f"part_{part}"][f"mark_{i}"]
+        for i in marksDict[f"semester_{semester}"]:
+            subCount=marksDict[f"semester_{semester}"][i]['Subject Count']
+            mark=marksDict[f"semester_{semester}"][i]['marks']
+            credit=marksDict[f"semester_{semester}"][i]['credits']  
+            sumOfMandCMultiplied=0
+            sumOfCredits=0
+
+            for j in range(subCount):
+                sumOfMandCMultiplied+=((mark[j]/10)*credit[j])
+                sumOfCredits+=credit[j]
+            gpa=sumOfMandCMultiplied/sumOfCredits
+
+            marksDict[f"semester_{semester}"][i]['gpa']=gpa
+
+        
+            
--- marks_class.py
+++ marks_class.py
@@ -25,13 +25,13 @@ class marks:
         partsCount=len(marksDict[f'semester_{semester}'])
         for i in range(1,partsCount+1):
             numberOfSubjects=int(input("how many subjects are in this part:"))
-            
+            mark=[]
+            credit=[]
             for j in range(1,numberOfSubjects+1):
-                mark=int(input(f"Enter the mark{j}:"))
-                credits=int(input(f"Enter the credits{j}:"))
-                marksDict[f"semester_{semester}"][f"part_{i}"][f"mark_{j}"]=mark
-                marksDict[f"semester_{semester}"][f"part_{i}"][f"credit_{j}"]=credits
-            marksDict[f"semester_{semester}"][f"part_{i}"]['grade']=0
-
-        print(marksDict)
+                mark.append(int(input(f"Enter the mark{j}:")))
+                credit.append(int(input(f"Enter the credits{j}:")))
+            marksDict[f"semester_{semester}"][f"part_{i}"]["Subject Count"]=numberOfSubjects    
+            marksDict[f"semester_{semester}"][f"part_{i}"]["marks"]=mark
+            marksDict[f"semester_{semester}"][f"part_{i}"]["credits"]=credit
+            
                        
